c04bd449dc4b5fd5696dadb6c2511c2a7668c267
ved-suiciniv <46250524+ved-suiciniv@users.noreply.github.com>
2020-09-22 11:54:36 +0100

    Update TicTacToe.cs
    Fixed some problens

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -5,83 +5,146 @@ namespace Tic_Tac_Toe
     class Program
     {
         public static char[,] game = new char[3, 3];
+        public static Boolean endGame = true;
+        public static int count = 1;
         static void Main(string[] args)
         {
             Boolean playGame = true;
             char player = 'X';
-            resetGame();
             //showNormal();
-            showGame();
-            while(playGame)
+            while (playGame)
             {
-
-                Boolean readingKey = true;
+                if(endGame == true || count == 10)
+                {
+                    //Console.WriteLine("Press any key to play!");
+                    //Console.Read();
+                    resetGame();
+                    count = 1;
+                    endGame = false;
+                }
+                showGame();
+                Boolean readingKey = true,test = true;
+                string local;
                 do
                 {
-                    Console.Write("Player({0}), Escolha o local:",player);
-                    string local = Console.ReadLine();
-                    if(local.Length == 1)
+                    do
                     {
-                        Console.WriteLine("Correto!");
-                        readingKey = false;
-                        if(player == 'X')
+                        Console.Write("Player({0}), Escolha o local:", player);
+                        local = Console.ReadLine();
+                        if (local.Length == 1)
                         {
-                            player = 'O';
+                            //Console.WriteLine("Correto!");
+                            readingKey = false;
                         }
-                        else
-                        {
-                            player = 'X';
-                        }
+                    } while (readingKey);
+                    test = changePosition(char.Parse(local), player);
+                    if(test == true)
+                    {
+                        Console.WriteLine("Please choice another position!");
                     }
-                } while (readingKey);
+
+                } while (test);
+                if (player == 'X')
+                {
+                    player = 'O';
+                }
+                else
+                {
+                    player = 'X';
+                }
             }
         }
-        public static Boolean changePosition(int position, char player)
+        public static void seachingWinner()
         {
-            char positionAux = 'N';
+            for(int i = 0;i < 3; i++)
+            {
+                if (game[i, 0] == game[i, 1] && game[i, 1] == game[i, 2])
+                {
+                    showGame();
+                    Console.WriteLine("Winner({0})", game[i, 0]);
+                    endGame = true;
+                    Console.WriteLine("Press any key to play!");
+                    Console.Read();
+                    return;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (game[0, i] == game[1, i] && game[1, i] == game[2, i])
+                {
+                    showGame();
+                    Console.WriteLine("Winner({0})", game[0, i]);
+                    endGame = true;
+                    Console.WriteLine("Press any key to play!");
+                    Console.Read();
+                    return;
+                }
+            }
+            if(game[0, 0] == game[1, 1] && game[1, 1] == game[2, 2] || game[2, 0] == game[1, 1] && game[1, 1] == game[0, 2])
+            {
+                showGame();
+                Console.WriteLine("Winner({0})", game[0, 0]);
+                endGame = true;
+                Console.WriteLine("Press any key to play!");
+                Console.Read();
+                return;
+            }
+        }
+        public static Boolean changePosition(char position, char player)
+        {
+            int[] positionAux = new int[2];
             switch (position)
             {
-                case 1:
-                    positionAux = game[0, 0];
+                case '1':
+                    positionAux[0] = 0;
+                    positionAux[1] = 0;
                     break;
-                case 2:
-                    positionAux = game[0, 1];
+                case '2':
+                    positionAux[0] = 0;
+                    positionAux[1] = 1;
                     break;
-                case 3:
-                    positionAux = game[0, 2];
+                case '3':
+                    positionAux[0] = 0;
+                    positionAux[1] = 2;
                     break;
-                case 4:
-                    positionAux = game[1, 0];
+                case '4':
+                    positionAux[0] = 1;
+                    positionAux[1] = 0;
                     break;
-                case 5:
-                    positionAux = game[1, 1];
+                case '5':
+                    positionAux[0] = 1;
+                    positionAux[1] = 1;
                     break;
-                case 6:
-                    positionAux = game[1, 2];
+                case '6':
+                    positionAux[0] = 1;
+                    positionAux[1] = 2;
                     break;
-                case 7:
-                    positionAux = game[2, 0];
+                case '7':
+                    positionAux[0] = 2;
+                    positionAux[1] = 0;
                     break;
-                case 8:
-                    positionAux = game[2, 1];
+                case '8':
+                    positionAux[0] = 2;
+                    positionAux[1] = 1;
                     break;
-                case 9:
-                    positionAux = game[2, 2];
-                    break;
-                default:
-                    // code block
+                case '9':
+                    positionAux[0] = 2;
+                    positionAux[1] = 2;
                     break;
             }
 
-            if(positionAux == 'O' && positionAux == 'X')
+            if(game[positionAux[0],positionAux[1]] == 'O' || game[positionAux[0], positionAux[1]] == 'X')
             {
-                return false;
+                return true;
             }
             else
             {
-                positionAux = player;
-                Console.WriteLine("Alteração({0})", positionAux);
-                return true;
+                char anterior = game[positionAux[0], positionAux[1]];
+                game[positionAux[0], positionAux[1]] = player;
+                Console.WriteLine("Player({0}), Alteração( de {1} para {2})", player,anterior, game[positionAux[0], positionAux[1]]);
+                count++;
+                seachingWinner();
+                return false;
             }
         }
         public static void resetGame()
